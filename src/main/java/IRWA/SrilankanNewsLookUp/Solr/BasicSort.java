2a6268d5f7ae70b654cccddc0b183b749225e8a2
nera94 <36783726+nera94@users.noreply.github.com>
2018-09-13 14:00:58 +0530

    Implement Query object with sorting and execute the query

--- src/main/java/IRWA/SrilankanNewsLookUp/Solr/BasicSort.java
+++ src/main/java/IRWA/SrilankanNewsLookUp/Solr/BasicSort.java
@@ -1,5 +1,42 @@
 package IRWA.SrilankanNewsLookUp.Solr;
 
+
+import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.SolrQuery.ORDER;
+import org.apache.solr.client.solrj.beans.DocumentObjectBinder;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.params.CommonParams;
+
+
 public class BasicSort {
+	
+	private static SolrClient client = null;
+
+	public static void main(String[] args) throws SolrServerException, IOException {
+		client = ApacheSolrClient.getInstance().getClient();
+
+		SolrQuery query = new SolrQuery();
+		query.set(CommonParams.Q, "*:*");
+		query.addSort(SolrConstant.id, ORDER.asc);
+		System.out.println("Query " + query.toQueryString());
+		SolrDocumentList solrDocs = executeQuery(query);
+		
+	}
+	
+	private static SolrDocumentList executeQuery(SolrQuery query) throws SolrServerException, IOException {
+		query.setRows(10);
+		QueryResponse response = client.query(query);
+		SolrDocumentList results = response.getResults();
+		System.out.println("No of documents found " + results.getNumFound());
+		return results;
+	}
+	
+	
 
 }
